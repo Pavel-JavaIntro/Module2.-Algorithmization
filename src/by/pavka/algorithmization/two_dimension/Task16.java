32412e209212d8502f217fbac5d7f09acf91ff02
Pavel-JavaIntro <62737771+Pavel-JavaIntro@users.noreply.github.com>
2020-04-03 20:03:44 +0300

    reformat tasks

--- src/by/pavka/algorithmization/two_dimension/Task16.java
+++ src/by/pavka/algorithmization/two_dimension/Task16.java
@@ -14,13 +14,14 @@ public class Task16 {
 	}
 	
 	public static int[][] formMagicSquare(int n) {
-		// Initializing a variable named n and assigning the integer 3 to it
+		
         
-        // Initializing a multi-dimensional array with 3 rows and 3 columns max
+        
         int m[][] = new int[n][n]; // 3 by 3
 
-        int i, j;
-        // Assigning 1 to variable num
+        int i; 
+	int j;
+        
         int num = 1;
 
         int nn = n * 3 / 2; //4
@@ -29,10 +30,8 @@ public class Task16 {
         for (i = 0; i < n; i++) {
             for (j = 0; j < n; j++) {
                 m[(j - i + nn) % n][(i * 2 - j + n) % n] = num++; 
-
-                System.out.println(num);
-            } // next j
-        } //next i
+            } 
+        } 
         
         Helper.printMatrix(m);
         return m;
